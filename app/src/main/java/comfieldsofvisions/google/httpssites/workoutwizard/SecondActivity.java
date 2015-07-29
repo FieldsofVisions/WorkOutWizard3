3899e0d69b27a3fd31232b247202e1b32e2e6903
Andre Fields <aundrey800@gmail.com>
2015-07-28 20:41:05 -0700

    Initial commit

---
+++ app/src/main/java/comfieldsofvisions/google/httpssites/workoutwizard/SecondActivity.java
@@ -0,0 +1,15 @@
+package comfieldsofvisions.google.httpssites.workoutwizard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andre on 7/26/2015.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
